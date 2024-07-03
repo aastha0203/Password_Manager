6e47d9a2bd5c217be67f49cf0b36d85ee799b0e8
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2024-07-03 20:09:38 +0530

    created the ui,add the functionality to save the passwords,site name and usrname to local storage ,added functionality to display data from local storage

--- passop/src/components/Navbar.jsx
+++ passop/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     
       <nav className='bg-slate-800 text-white '>
-        <div className='mycontainer flex justify-between items-center px-4 py-5 h-16'>
+        <div className='mycontainer flex justify-between items-center px-4 py-5 h-18'>
         <div className="logo font-bold text-white text-2xl">
         <span className='text-green-700 '>&lt;
             </span>
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
 
         <ul>
-            <li className='flex gap-4'>
+            <li className='flex gap-16'>
                 <a className='hover:font-bold'href='/'>Home</a>
                 <a className='hover:font-bold'href='#'>About</a>
                 <a className='hover:font-bold'href='#'>Contact</a>
